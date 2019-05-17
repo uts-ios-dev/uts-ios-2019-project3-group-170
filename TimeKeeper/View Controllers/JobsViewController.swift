afccd09d38b8065d91af8a580dbbb930e5c84982
Kieran O'Brien <12004507@student.uts.edu.au>
2019-05-17 12:38:16 +1000

    Added and connected view controllers

---
+++ TimeKeeper/View Controllers/JobsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  JobsViewController.swift
+//  TimeKeeper
+//
+//  Created by Kieran O'Brien on 17/5/19.
+//  Copyright © 2019 Trent Diamond. All rights reserved.
+//
+
+import UIKit
+
+class JobsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
---
+++ TimeKeeper/View Controllers/ReportsViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ReportsViewController.swift
+//  TimeKeeper
+//
+//  Created by Kieran O'Brien on 17/5/19.
+//  Copyright © 2019 Trent Diamond. All rights reserved.
+//
+
+import UIKit
+
+class ReportsViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    
+}
