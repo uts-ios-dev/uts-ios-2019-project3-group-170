1a533ab3b9100853158b06a0eaace9b15427455d
Kieran O'Brien <12004507@student.uts.edu.au>
2019-05-20 17:31:01 +1000

    Changed Settings View Controller

--- TimeKeeper/View Controllers/SettingsViewController.swift
+++ TimeKeeper/View Controllers/SettingsViewController.swift
@@ -12,7 +12,7 @@ class SettingsViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
     
     func loadSettings() -> Settings? {
