f7333348ddf886fb6fdac7cd5153bf7455475e9b
Kieran O'Brien <12004507@student.uts.edu.au>
2019-05-22 10:19:26 +1000

    Added missing view controllers

---
+++ TimeKeeper/View Controllers/NewTimeEntryViewController.swift
@@ -0,0 +1,20 @@
+//
+//  NewTimeEntryViewController.swift
+//  TimeKeeper
+//
+//  Created by Kieran O'Brien on 22/5/19.
+//  Copyright © 2019 Trent Diamond. All rights reserved.
+//
+
+import UIKit
+
+class NewTimeEntryViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Make a popup asking which job they would like to log their time against
+    }
+    
+    
+}
