43f4511c18b87cc8c3ac17090aaea0a9ce5014cf
Kieran O'Brien <12004507@student.uts.edu.au>
2019-05-25 12:33:57 +1000

    Half completed edit time view controller
    Fixed issues with settings view controller

--- TimeKeeper/View Controllers/EditTimeEntryViewController.swift
+++ TimeKeeper/View Controllers/EditTimeEntryViewController.swift
@@ -3,7 +3,7 @@
 //  TimeKeeper
 //
 //  Created by Kieran O'Brien on 22/5/19.
-//  Copyright © 2019 Trent Diamond. All rights reserved.
+//  Copyright © 2019 Kieran O'Brien. All rights reserved.
 //
 
 import UIKit
@@ -15,5 +15,52 @@ class EditTimeEntryViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
+    @IBOutlet weak var timeLabel: UILabel!
+    @IBOutlet weak var originalStartTimeLabel: UILabel!
+    @IBOutlet weak var originalFinishTimeLabel: UILabel!
     
+    @IBOutlet weak var startTimeDatePicker: UIDatePicker!
+    @IBOutlet weak var endTimeDatePicker: UIDatePicker!
+    
+    var timeEntry: TimeEntry?
+    
+    @IBAction func startDateTimeChanged(_ sender: UIDatePicker) {
+        print("User has changed the start time")
+        updateTimeLabel()
+    }
+    
+    @IBAction func endDateTimeChanged(_ sender: UIDatePicker) {
+        print("User has changed the end time")
+        updateTimeLabel()
+    }
+    
+    // Calculate the amount of hours and mintues betweent the start and end times
+    func updateTimeLabel() {
+        let dateString1 = startTimeDatePicker.date
+        let dateString2 = endTimeDatePicker.date
+        
+        let calendar = Calendar.current
+        
+        let timeDifference = calendar.dateComponents([.minute], from: dateString1, to: dateString2)
+        
+        let hours = timeDifference.minute! / 60
+        let minutes = timeDifference.minute!
+        
+        formatTimeLabel(hours: hours, minutes: minutes)
+
+    }
+    
+    // Format the amount of hours and minutes into a string
+    func formatTimeLabel(hours: Int, minutes: Int) {
+        var label: String = ""
+        if hours > 99 {
+            label += "<99h "
+        } else {
+            label += "\(hours)h "
+        }
+        
+        label += "\(minutes % 60)min"
+        
+        timeLabel.text = label
+    }
 }
