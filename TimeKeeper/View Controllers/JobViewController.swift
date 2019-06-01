074be2ba5f8e19bc76fb4bfb0fb20d6c54d1c1c5
Kieran O'Brien <12004507@student.uts.edu.au>
2019-06-01 15:25:31 +1000

    added functionality to Job View Controller

--- TimeKeeper/View Controllers/JobViewController.swift
+++ TimeKeeper/View Controllers/JobViewController.swift
@@ -12,8 +12,11 @@ class JobViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        // lad the jobs from storage and list them in the table
+        // load the jobs from storage and list them in the table
+        jobs = loadJobs()
     }
+    
+    var jobs: [Job]?
     
     @IBOutlet weak var jobsTable: UITableView!
     
@@ -24,7 +27,12 @@ class JobViewController: UIViewController, UITableViewDelegate, UITableViewDataS
     
     // Set the number of rows to be the amount of jobs
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        if jobs!.count > 0 {
+            return jobs!.count
+        } else {
+            emptyMessage(message: "You don't have any jobs yet.\nYou can create up to 10.", viewController: jobsTable)
+            return 0
+        }
     }
     
     // Format the row cells with the prototype cell class ScoreTableViewCell
@@ -32,5 +40,29 @@ class JobViewController: UIViewController, UITableViewDelegate, UITableViewDataS
         return JobTableViewCell()
     }
     
+    func emptyMessage(message:String, viewController:UITableView) {
+        let rect = CGRect(origin: CGPoint(x: 0,y :0), size: CGSize(width: self.view.bounds.size.width, height: self.view.bounds.size.height))
+        let messageLabel = UILabel(frame: rect)
+        messageLabel.text = message
+        messageLabel.textColor = UIColor.black
+        messageLabel.numberOfLines = 0;
+        messageLabel.textAlignment = .center;
+        messageLabel.font = UIFont(name: "TrebuchetMS", size: 15)
+        messageLabel.sizeToFit()
+        
+        viewController.backgroundView = messageLabel;
+        viewController.separatorStyle = .none;
+    }
+    
+    func loadJobs() -> [Job]? {
+        let dataStorage = DataStorage()
+        
+        do {
+            return try dataStorage.loadJobs()
+        } catch {
+            print("There were no jobs to load.")
+            return nil
+        }
+    }
     
 }
